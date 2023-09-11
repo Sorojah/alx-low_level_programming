4d1f13934ca925d80654ae4412511b4f2ceb58c5
Magnate Jude <magnatejude@gmail.com>
2023-09-11 05:22:31 -0700

    0x01. C - Variables, if, else, while (Mandatory)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{
+putchar (upperCase);
+upperCase += 1;
+}
+putchar ('\n');
+return (0);
+}
